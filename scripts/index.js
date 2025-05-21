ebc375d981a9cae209c32aabe657c96daf972fc3
Spartan <mrkillerrate775@gmail.com>
2025-05-21 21:19:19 +0300

    Написал код, всё работает

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,37 @@
 
 // @todo: DOM узлы
 
+const placeContainer = document.querySelector('.places__list');
+// const addButton = document.querySelector('.profile__add-button');
+
+
 // @todo: Функция создания карточки
+
+function addCard(titleValue, imageSrc, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    cardElement.querySelector('.card__title').textContent = titleValue;
+    cardElement.querySelector('.card__image').src = imageSrc;
+    placeContainer.addEventListener('click', deleteCard)
+    return cardElement;
+}
+
+// addButton.addEventListener('click', function () {
+//     placeContainer.append(addCard());
+// });
 
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+    if (evt.target.classList.contains('card__delete-button')) {
+        const card = evt.target.closest('.card');
+        card.remove();
+    }
+}
+
 // @todo: Вывести карточки на страницу
+
+for (let i = 0; i < initialCards.length; i++) {
+placeContainer.append(addCard(initialCards[i].name, initialCards[i].link, deleteCard))
+}
